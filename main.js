73a3584e3f1249c481113abf2a598ee8c3fffad3
Dmitry Dolgushin <razor9714@gmail.com>
2020-11-30 17:35:44 +0500

    Homework 7

--- main.js
+++ main.js
@@ -1,81 +1,97 @@
 import { random, buttonClickCounter, getElById } from './utils.js'
 import Pokemon from './pokemon.js'
 import createLog from './logs.js'
-import { pokemons } from './pokemons.js'
 
-const $control = document.querySelector('.control')
-const $btnStart = document.createElement('button')
-$btnStart.classList.add('button')
-$btnStart.innerText = 'Начать игру'
-$control.appendChild($btnStart)
+class Game {
+  getPokemons = async () => {
+    const response = await fetch('https://reactmarathon-api.netlify.app/api/pokemons')
+    const body = await response.json()
+    return body
+  }
 
-const resetGame = () => {
-  const allButtons = document.querySelectorAll('.control .button')
-  allButtons.forEach(($item) => $item.remove())
-  $control.appendChild($btnStart)
-}
+  getDamage = async (id1, id2, idAtck) => {
+    const responce = await fetch(
+      `https://reactmarathon-api.netlify.app/api/fight?player1id=${id1}&attackId=${idAtck}&player2id=${id2}`,
+    )
 
-const autoKick = (player1, player2) => {
-  player1.changeHP(
-    random(player2.attacks[0].maxDamage, player2.attacks[0].minDamage),
-    (count, damageHP, defaultHP) => {
-      createLog(player1, player2, count, damageHP, defaultHP)
+    const body = await responce.json()
+    return body
+  }
 
-      if (damageHP === 0) {
-        resetGame()
-      }
-    },
-  )
-}
+  start = async () => {
+    const $control = document.querySelector('.control')
+    const $btnStart = document.createElement('button')
+    $btnStart.classList.add('button')
+    $btnStart.innerText = 'Начать игру'
+    $control.appendChild($btnStart)
 
-$btnStart.addEventListener('click', () => {
-  $btnStart.remove()
+    const resetGame = () => {
+      const allButtons = document.querySelectorAll('.control .button')
+      allButtons.forEach(($item) => $item.remove())
+      $control.appendChild($btnStart)
+    }
 
-  // TODO: это надо куда-то вынести
-  const characterPokemon = pokemons[random(pokemons.length - 1, 0)]
-  const enemyPokemons = pokemons.filter((pok) => pok.name !== characterPokemon.name)
-  const enemyPokemon = enemyPokemons[random(enemyPokemons.length - 1, 0)]
+    $btnStart.addEventListener('click', async () => {
+      $btnStart.remove()
 
-  const $characterImg = getElById('sprite-character')
-  $characterImg.src = characterPokemon.img
+      const pokemons = await this.getPokemons()
+      const characterPokemon = pokemons[random(pokemons.length - 1, 0)]
+      const enemyPokemons = pokemons.filter((pok) => pok.name !== characterPokemon.name)
+      const enemyPokemon = enemyPokemons[random(enemyPokemons.length - 1, 0)]
 
-  const $characterName = getElById('name-character')
-  $characterName.innerText = characterPokemon.name
+      const $characterImg = getElById('sprite-character')
+      $characterImg.src = characterPokemon.img
 
-  const $enemyImg = getElById('sprite-enemy')
-  $enemyImg.src = enemyPokemon.img
+      const $characterName = getElById('name-character')
+      $characterName.innerText = characterPokemon.name
 
-  const $enemyName = getElById('name-enemy')
-  $enemyName.innerText = enemyPokemon.name
-  // -------------
+      const $enemyImg = getElById('sprite-enemy')
+      $enemyImg.src = enemyPokemon.img
 
-  const player1 = new Pokemon({
-    ...characterPokemon,
-    selectors: 'character',
-  })
+      const $enemyName = getElById('name-enemy')
+      $enemyName.innerText = enemyPokemon.name
 
-  let player2 = new Pokemon({
-    ...enemyPokemon,
-    selectors: 'enemy',
-  })
-
-  player1.attacks.forEach((item) => {
-    const $btn = document.createElement('button')
-    $btn.classList.add('button')
-    $btn.innerText = item.name
-
-    const btnCount = buttonClickCounter(item.maxCount, $btn)
-
-    $btn.addEventListener('click', () => {
-      player2.changeHP(random(item.maxDamage, item.minDamage), (count, damageHP, defaultHP) => {
-        createLog(player2, player1, count, damageHP, defaultHP)
+      const player1 = new Pokemon({
+        ...characterPokemon,
+        selectors: 'character',
       })
 
-      autoKick(player1, player2)
+      let player2 = new Pokemon({
+        ...enemyPokemon,
+        selectors: 'enemy',
+      })
 
-      btnCount()
+      player1.attacks.forEach((item) => {
+        const $btn = document.createElement('button')
+        $btn.classList.add('button')
+        $btn.innerText = item.name
+
+        const btnCount = buttonClickCounter(item.maxCount, $btn)
+
+        $btn.addEventListener('click', async () => {
+          const damage = await this.getDamage(characterPokemon.id, enemyPokemon.id, item.id)
+          const damageResult = damage.kick
+
+          player2.changeHP(damageResult.player2, (count, damageHP, defaultHP) => {
+            createLog(player2, player1, count, damageHP, defaultHP)
+          })
+
+          player1.changeHP(damageResult.player1, (count, damageHP, defaultHP) => {
+            createLog(player1, player2, count, damageHP, defaultHP)
+
+            if (damageHP === 0) {
+              resetGame()
+            }
+          })
+
+          btnCount()
+        })
+
+        $control.appendChild($btn)
+      })
     })
+  }
+}
 
-    $control.appendChild($btn)
-  })
-})
+const game = new Game()
+game.start()
